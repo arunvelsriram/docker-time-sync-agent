9fd2cde2ebfb117c1a7fb430f6f58490a07010ba
Arunvel Sriram <arunvelsriram@gmail.com>
2016-12-25 15:28:16 +0530

    Remove unused code, comments

--- docker-time-sync/main.m
+++ docker-time-sync/main.m
@@ -21,102 +21,42 @@
 #include <Foundation/NSString.h>
 #include <Foundation/NSArray.h>
 
-io_connect_t  root_port; // a reference to the Root Power Domain IOService
-
-void
-MySleepCallBack( void * refCon, io_service_t service, natural_t messageType, void * messageArgument )
+void SleepCallBack( void * refCon, io_service_t service, natural_t messageType, void * messageArgument )
 {
-    printf( "messageType %08lx, arg %08lx\n",
+    NSLog( @"messageType %08lx, arg %08lx\n",
            (long unsigned int)messageType,
            (long unsigned int)messageArgument );
     
-    NSTask *task = [[NSTask alloc] init];
-    NSArray *arguments = [NSArray arrayWithObjects:@"run", @"--rm", @"-it", @"--privileged", @"--pid=host", @"walkerlee/nsenter", [NSString stringWithFormat: @"%@ %@",@"-t",@"1"], @"-m", @"-u", @"-i", @"-n", @"ntpd", @"-d", @"-q", @"-n", @"-p", @"pool.ntp.org", nil];
-    [task setLaunchPath: @"/usr/local/bin/docker"];
-    [task setArguments: arguments];
-    
-    switch ( messageType )
-    {
-            
-        case kIOMessageCanSystemSleep:
-            /* Idle sleep is about to kick in. This message will not be sent for forced sleep.
-             Applications have a chance to prevent sleep by calling IOCancelPowerChange.
-             Most applications should not prevent idle sleep.
-             
-             Power Management waits up to 30 seconds for you to either allow or deny idle
-             sleep. If you don't acknowledge this power change by calling either
-             IOAllowPowerChange or IOCancelPowerChange, the system will wait 30
-             seconds then go to sleep.
-             */
-            
-            //Uncomment to cancel idle sleep
-            //IOCancelPowerChange( root_port, (long)messageArgument );
-            // we will allow idle sleep
-            IOAllowPowerChange( root_port, (long)messageArgument );
-            NSLog(@"hello 1");
-            break;
-            
-        case kIOMessageSystemWillSleep:
-            /* The system WILL go to sleep. If you do not call IOAllowPowerChange or
-             IOCancelPowerChange to acknowledge this message, sleep will be
-             delayed by 30 seconds.
-             
-             NOTE: If you call IOCancelPowerChange to deny sleep it returns
-             kIOReturnSuccess, however the system WILL still go to sleep.
-             */
-            
-            IOAllowPowerChange( root_port, (long)messageArgument );
-            NSLog(@"hello2");
-            break;
-            
-        case kIOMessageSystemWillPowerOn:
-            //System has started the wake up process...
-            NSLog(@"hello3");
-            break;
-            
-        case kIOMessageSystemHasPoweredOn:
-            //System has finished waking up...
-            NSLog(@"hello4");
-            [task launch];
-            break;
-            
-        default:
-            break;
+    if (messageType == kIOMessageSystemHasPoweredOn) {
+        NSLog(@"Started to sync time...");
+        // docker run --rm -it --privileged --pid=host walkerlee/nsenter -t 1 -m -u -i -n ntpd -d -q -n -p pool.ntp.org
+        NSTask *task = [[NSTask alloc] init];
+        NSArray *arguments = [NSArray arrayWithObjects:@"run", @"--rm", @"-it", @"--privileged", @"--pid=host", @"walkerlee/nsenter", [NSString stringWithFormat: @"%@ %@",@"-t",@"1"], @"-m", @"-u", @"-i", @"-n", @"ntpd", @"-d", @"-q", @"-n", @"-p", @"pool.ntp.org", nil];
+        [task setLaunchPath: @"/usr/local/bin/docker"];
+        [task setArguments: arguments];
+        task.terminationHandler = ^(NSTask *aTask){
+            NSLog(@"\nDone!\n");
+        };
+        [task launch];
     }
 }
 
 
 int main( int argc, char **argv )
 {
-    // notification port allocated by IORegisterForSystemPower
-    IONotificationPortRef  notifyPortRef;
+    io_connect_t  root_port;
+    IONotificationPortRef notifyPortRef;
+    io_object_t notifierObject;
+    void* refCon;
     
-    // notifier object, used to deregister later
-    io_object_t            notifierObject;
-    // this parameter is passed to the callback
-    void*                  refCon;
-    
-    // register to receive system sleep notifications
-    
-    root_port = IORegisterForSystemPower( refCon, &notifyPortRef, MySleepCallBack, &notifierObject );
-    if ( root_port == 0 )
+    root_port = IORegisterForSystemPower(refCon, &notifyPortRef, SleepCallBack, &notifierObject);
+    if (root_port == 0)
     {
         printf("IORegisterForSystemPower failed\n");
         return 1;
     }
     
-    // add the notification port to the application runloop
-    CFRunLoopAddSource( CFRunLoopGetCurrent(),
-                       IONotificationPortGetRunLoopSource(notifyPortRef), kCFRunLoopCommonModes );
-    
-    /* Start the run loop to receive sleep notifications. Don't call CFRunLoopRun if this code
-     is running on the main thread of a Cocoa or Carbon application. Cocoa and Carbon
-     manage the main thread's run loop for you as part of their event handling
-     mechanisms.
-     */
+    CFRunLoopAddSource(CFRunLoopGetCurrent(),
+                       IONotificationPortGetRunLoopSource(notifyPortRef), kCFRunLoopCommonModes);
     CFRunLoopRun();
-    
-    // Not reached, CFRunLoopRun doesn't return in this case.
-    // return (0);
 }
-
