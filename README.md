2886d819286bf43a8c507d54fcabe009f78ff917
Arunvel Sriram <arunvelsriram@gmail.com>
2016-12-26 04:30:09 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# docker-time-sync-agent
+Tool to prevent time drift in Docker for Mac's VM.
---
+++ docker-time-sync/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  docker-time-sync
+//
+//  Created by ArunvelSriram on 25/12/16.
+//  Copyright © 2016 ArunvelSriram. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
