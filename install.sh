a67c60306e8fa444b4a897881e117293a1e347fc
Arunvel Sriram <arunvelsriram@gmail.com>
2016-12-27 00:05:18 +0530

    Automate installation and launchd setup

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+tag=$(git tag | xargs | awk '{print $1}')
+username=$(id -u -n)
+plist_filename="io.github.arunvelsriram.docker-time-sync-agent.plist"
+
+pushd /tmp
+echo
+echo Setting up scrips and binary files...
+curl -LO https://github.com/arunvelsriram/docker-time-sync-agent/releases/download/$tag/Binaries.zip
+unzip Binaries.zip
+rm -f Binaries.zip
+mv Binaries/update-docker-time Binaries/docker-time-sync-agent /usr/local/bin
+rm -rf Binaries
+echo
+echo Setting up Launch Agent...
+curl -O https://raw.githubusercontent.com/arunvelsriram/docker-time-sync-agent/master/$plist_filename
+sed -i .backup "s/YOUR_USERNAME/$username/g" $plist_filename
+cp $plist_filename ~/Library/LaunchAgents
+launchctl unload ~/Library/LaunchAgents/$plist_filename
+launchctl load ~/Library/LaunchAgents/$plist_filename
+popd
+
+echo
+echo "Logs can be viewed in Console.app or ~/.docker-time-sync-agent.log"
+echo All Done!
