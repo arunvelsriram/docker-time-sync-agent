f48d197c6e67ab4bfc4b04fd1c36ca6b0ceb295c
Arunvel Sriram <arunvelsriram@gmail.com>
2017-03-13 23:44:32 +0530

    Fix #4 supress curl output in installation script

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 set -e
 
 latest_release_tag() {
-	curl https://api.github.com/repos/arunvelsriram/docker-time-sync-agent/releases/latest \
+	curl -s https://api.github.com/repos/arunvelsriram/docker-time-sync-agent/releases/latest \
 	| python -c "import sys, json; print json.load(sys.stdin)['tag_name']"
 }
 
@@ -20,14 +20,14 @@ echo_me "Preparing to install $tag_name..."
 pushd /tmp
 
 echo_me "Setting up scripts and binary files..."
-curl -LO https://github.com/arunvelsriram/docker-time-sync-agent/releases/download/$tag_name/Binaries.zip
+curl -LOs https://github.com/arunvelsriram/docker-time-sync-agent/releases/download/$tag_name/Binaries.zip
 unzip Binaries.zip
 rm -f Binaries.zip
 mv Binaries/update-docker-time Binaries/docker-time-sync-agent /usr/local/bin
 rm -rf Binaries
 
 echo_me "Setting up Launch Agent..."
-curl -O https://raw.githubusercontent.com/arunvelsriram/docker-time-sync-agent/master/$plist_filename
+curl -Os https://raw.githubusercontent.com/arunvelsriram/docker-time-sync-agent/master/$plist_filename
 sed -i .backup "s/YOUR_USERNAME/$username/g" $plist_filename
 cp $plist_filename ~/Library/LaunchAgents
 launchctl unload ~/Library/LaunchAgents/$plist_filename 2>/dev/null
